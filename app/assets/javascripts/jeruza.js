55a591777ebe4e2001bdc1f175ae8e8cd21e1735
Victor Vitalino <vitalino.victor@gmail.com>
2015-06-18 14:09:17 -0300

    Info form

--- app/assets/javascripts/jeruza.js
+++ app/assets/javascripts/jeruza.js
@@ -1,3 +1,19 @@
 //= require jeruza/js/script
 //= require jeruza/js/jquery.flexslider
 //= require jeruza/js/slider
+  jQuery(document).ready(function($){
+    var mes = 'Mai';
+    var botao = '';
+    var current = mes;
+    $('#'+ mes).css('display','block');
+
+    $('.tab-mes span').click(function(){
+      botao = $(this).attr('data-mes');
+      if(current!=botao){
+        $('.s-info').css('display','none');
+        $('#'+ botao).css('display','block');
+        current = botao;
+      }
+    });
+
+  });
